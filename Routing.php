bc37d68776d35e578acccd3c6bb618a1d1103a0b
Mangner <05langner20@gmail.com>
2025-10-29 02:42:10 +0100

    Login view updated

--- Routing.php
+++ Routing.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'src/controllers/SecurityController.php';
-require_once 'src/controllers/DashboardController.php'
+require_once 'src/controllers/DashboardController.php';
 
 class Routing {
 
@@ -13,13 +13,13 @@ class Routing {
             'controller' => "SecurityController",
             'action' => 'register'
         ],
-        [
-            'dashboard' => [
-                'controller' => "DashboardController",
-                'action' => 'index'
-            ]
+        'dashboard' => [
+            'controller' => "DashboardController",
+            'action' => 'index'
         ]
     ];
+
+
     public static function run(string $path) {
     switch($path) {
         case 'dashboard':
